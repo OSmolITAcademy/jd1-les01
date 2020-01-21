fd0221a2d863be24577af0d85969a88f8bec375d
Ol Smol <olga.smolyakova.work@gmail.com>
2020-01-21 16:21:50 +0300

    first commit message

---
+++ src/by/htp/les01/main/First.java
@@ -0,0 +1,15 @@
+package by.htp.les01.main;
+
+public class First {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, world!!!");
+		System.out.print(" Hello, world!!! ");
+		System.out.print(" Hello, world!!! ");
+		System.out.print(" Hello, world!!! ");
+		
+		int myVariable;
+		
+	}
+
+}
---
+++ src/by/htp/les01/service/Second.java
@@ -0,0 +1,13 @@
+package by.htp.les01.service;
+
+public class Second {
+
+}
+
+// by.htp.les_01.service.Second
+// by.htp.les01.main.Second
+
+class MyClass {
+	
+	
+}
